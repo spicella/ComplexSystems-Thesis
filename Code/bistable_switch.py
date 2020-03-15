885af4841acb294f67f4deb3eba9310dbc333437
Sergio <sergiopicella@gmail.com>
2020-03-15 01:21:22 +0100

    ...fast, finally!
    Fast computational times and apparently working..

--- Code/bistable_switch.py
+++ Code/bistable_switch.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[86]:
 
 
 import numpy as np
+import scipy
 import matplotlib.pyplot as plt
 from matplotlib import gridspec
 from mpl_toolkits.axes_grid1 import make_axes_locatable
@@ -32,14 +33,14 @@ else:
     print ("Successfully created the directory %s " % results_dir)
 
 
-# In[30]:
+# In[74]:
 
 
-def convert_to_df(mat): 
+def convert_to_df(mat,dtype="float32"): 
     #dataframes were used as they allowed computational times almost
     #as fast as numpy but with easier syntax/more useful built in functions 
     #for the present algorithm
-    return pd.DataFrame(mat)
+    return pd.DataFrame(np.float32(mat))
 
 def makeGaussian(size = 100, fwhm = 50, center=None):
     """ Make a square gaussian kernel 2d."""
@@ -60,9 +61,9 @@ def get_distrib_mat(grid):
     """Given a grid (of PDF), returns values of the grid (n) steps in a given"""
     """direction of the grid."""
     df_grid = convert_to_df(grid)
-    N = df_grid.shift(1,axis=0,fill_value=0)  #looks 1 cell up
+    N = df_grid.shift(-1,axis=0,fill_value=0)  #looks 1 cell up
     E = df_grid.shift(-1,axis=1,fill_value=0) #looks 1 cell right
-    S = df_grid.shift(-1,axis=0,fill_value=0) #looks 1 cell down 
+    S = df_grid.shift(1,axis=0,fill_value=0) #looks 1 cell down 
     W = df_grid.shift(1,axis=1,fill_value=0)  #looks 1 cell left 
     return N,E,S,W
 
@@ -92,8 +93,8 @@ def r4(i,j):
 #Note that these coefficients are fixed during the exp, can be evaluated even just once!
 
 #all weights for the reactions, here just examples to picture dynamics of time evolution
-size = 5
-a = np.zeros(shape=[4,size,size])
+size = 300
+a = np.zeros(shape=[4,size,size],dtype="float32")
 
 for i in range(0,size):
     for j in range(0,size):
@@ -109,7 +110,7 @@ a_df.append(convert_to_df(a[2,:,:]))
 a_df.append(convert_to_df(a[3,:,:]))
 
 
-# In[35]:
+# In[53]:
 
 
 #of each propensity grid, NESW
@@ -120,11 +121,17 @@ a2_df = get_distrib_mat(a_df[2])
 a3_df = get_distrib_mat(a_df[3])
 
 
-# In[36]:
+# In[55]:
+
+
+type(a0_df[1].iloc[0][0])
+
+
+# In[56]:
 
 
 #test!
-grid = makeGaussian(size=5,center=[1,3],fwhm=2)
+grid = makeGaussian(size=10,center=[1,3],fwhm=2)
 plt.matshow(grid)
 shifted_distrib = get_distrib_mat(grid)
 N = shifted_distrib[0]
@@ -133,40 +140,41 @@ S = shifted_distrib[2]
 W = shifted_distrib[3]
 
 
-# In[52]:
+# In[57]:
 
 
-a0_df[3]*W-a_df[0]*grid
+#correct, consider inverted axis on plot
+plt.matshow(S)
 
 
-# In[55]:
+# In[ ]:
 
 
-a2_df[0]*N
 
 
-# In[32]:
+
+# In[13]:
 
 
 plt.matshow(a_df[0]+a_df[1]+a_df[2]+a_df[3])
-plt.title("Reaction weights",y=1.1)
+plt.title("Propensity weights",y=1.1)
 plt.colorbar()
 plt.show()
 
 
-# In[31]:
+# In[61]:
 
 
 #Coefficients matrices
 for i in range(0,4):
     plt.matshow(a_df[i])
-    plt.title("Reaction %d weights"%(i+1),y=1.1)
+    plt.title("Propensity weight of %d reaction"%(i+1),y=1.1)
     plt.colorbar()
     plt.show()
     #plt.clim(0, .5);
 
 
-# In[7]:
+# In[17]:
 
 
 def create_size_folderdata(size):
@@ -180,9 +188,9 @@ def create_size_folderdata(size):
 
 #create_name/get data functions
 
-def name_data(t_end):
+def name_data(t_end,dt):
     """Create name of output file given simulation parameters"""
-    data_name = "data_size%d_x0y0=%d_%d_std=%d_t_end=%d.dat"%(size,center[0],center[1],std,t_end)
+    data_name = "data_size%d_x0y0=%d_%d_std=%d_dt=%.5f_t_end=%d.dat"%(size,center[0],center[1],std,dt,t_end)
     return data_name
 
 def get_data(output_path,t_end):
@@ -212,7 +220,13 @@ def get_configuration(data):
     return convert_to_df(np.asarray(data).reshape(size,size))
 
 
-# In[67]:
+# In[40]:
+
+
+
+
+
+# In[136]:
 
 
 def time_ev_mat(grid,t_end=500,dt=.1):
@@ -221,13 +235,14 @@ def time_ev_mat(grid,t_end=500,dt=.1):
     start_t = time.clock()
     t = 0  
     size = np.shape(grid)[0]
-    a = np.zeros(shape=[4,size,size])
-    Id_mat = np.eye(size)
-    full1_mat = np.full([size,size],1)
+    dt_mat = np.full([size,size],fill_value=dt,dtype="float32")
+
+    a = np.zeros(shape=[4,size,size],dtype="float32")
+    
     
     result_path_name = create_size_folderdata(size)
     
-    filePath = result_path_name+"/"+name_data(t_end)
+    filePath = result_path_name+"/"+name_data(t_end,dt)
  
     #to avoid overwrite
     if os.path.exists(filePath):
@@ -255,22 +270,26 @@ def time_ev_mat(grid,t_end=500,dt=.1):
     a1_df = get_distrib_mat(a_df[1])
     a2_df = get_distrib_mat(a_df[2])
     a3_df = get_distrib_mat(a_df[3])
-
-    
-    t_flag=40
+    increment = np.zeros([size,size])
     tgrid_saved = []
-    t_save_every = t_end/(dt*100) #save only n configurations
+    t_save_every = t_end/(dt*50) #save only n configurations
     saved_counter = 0
     while t<int(t_end/dt): #not to lose last step
         #running flag
-        if(t%int(t_end/(t_flag*dt)))==0:
+        if(t%int(t_end/(10000*dt)))==0: #display every n
+            #print(type(grid.iloc[0][1]))
+            print("Effective time = %.4f\n"%(dt*t))
             print("Process @%d/%d (%.2f/100)"%(t,t_end/dt,(100*(t*dt)/t_end)))
             print("Normalized sum of grid %.8f"%(sum(sum(grid.values))/(size*size)))
-            print("Elapsed time = %.3fseconds\n"%(time.clock()-start_t))    
+            print("Elapsed time = %.3fseconds\n"%(time.clock()-start_t))  
+            
+            plt.contourf(grid)
+            plt.colorbar()
+            plt.show()
             #not to lose first step
         #here configurations are saved as 1D arrays, one per each time step
         if(t%t_save_every)==0:
-            (convert_to_df(grid.values.flatten()).T).to_csv(result_path_name+"/"+name_data(t_end),header=None,index=None,sep=",",float_format='%f',mode='a')
+            (convert_to_df(grid.values.flatten()).T).to_csv(result_path_name+"/"+name_data(t_end,dt),header=None,index=None,sep=",",float_format='%f',mode='a')
             saved_counter+= 1
             tgrid_saved.append((convert_to_df(grid.values.flatten()).T))
         #working directly on matrices to avoid nested for loops with plenty fo evaluations
@@ -282,32 +301,37 @@ def time_ev_mat(grid,t_end=500,dt=.1):
         W = shifted_distrib[3]
                 
         #evaluate increment
-        increment = np.zeros(shape=[size,size])
-
+        increment = np.zeros(shape=[size,size],dtype="float32")
+        incr1 = np.subtract(np.multiply(a0_df[3],W),np.multiply(a_df[0],grid))
+        incr2 = np.subtract(np.multiply(a1_df[1],E),np.multiply(a_df[1],grid))
+        incr3 = np.subtract(np.multiply(a2_df[0],N),np.multiply(a_df[2],grid))
+        incr4 = np.subtract(np.multiply(a3_df[2],S),np.multiply(a_df[3],grid))
         
-        increment += a0_df[3]*W-a_df[0]*grid
-        increment += a1_df[1]*W-a_df[1]*grid
-        increment += a2_df[0]*N-a_df[2]*grid
-        increment += a3_df[2]*S-a_df[3]*grid
-        increment *= dt
+        
+        increment = np.add(increment, incr1)
+        increment = np.add(increment, incr2)
+        increment = np.add(increment, incr3)
+        increment = np.add(increment, incr4)
+        
+        increment = np.multiply(increment,dt_mat)
         
         #implement mask for thresholds
         
         
-        grid = grid + increment
+        grid = np.add(grid,increment)
         t+=1
 
     end_t = time.clock()
     print("\n\nDone in %.3fseconds"%(end_t-start_t))  
-    file = open(result_path_name+"/"+"Stats_size=%d_center=(%d_%d)_std=%d_t_end=%d.txt"%(size,center[0],center[1],std,t_end),"w") 
+    file = open(result_path_name+"/"+"Stats_size=%d_center=(%d_%d)_std=%d_dt=%.5f_t_end=%d.txt"%(size,center[0],center[1],std,dt,t_end),"w") 
     file.write("Done in %.3fseconds"%(end_t-start_t)) 
      
     print("Now some live plots")
 
     fig, axes = plt.subplots(nrows=1, ncols=4,figsize=(20,5))
-    ncolors = 15
+    ncolors = 20
     cmap ="jet"
-    fig.suptitle("Configurations for %d x %d grid, $(x_{0},y_{0})=(%d,%d)$, $\\sigma=%d, t_{end}=%d$"%(size,size,center[0],center[1],std,t_end),fontsize=20,y=1.)
+    fig.suptitle("Configurations for %d x %d grid, $(x_{0},y_{0})=(%d,%d)$, $\\sigma=%d, t_{end}=%d$, dt=%.4f"%(size,size,center[0],center[1],std,t_end,dt),fontsize=20,y=1.)
 
     im0 = axes[0].contourf(get_configuration(tgrid_saved[0]),ncolors,cmap=cmap) # n equally spaced lines
     divider = make_axes_locatable(axes[0])
@@ -345,12 +369,18 @@ def time_ev_mat(grid,t_end=500,dt=.1):
     axes[3].set_xlabel("A species",fontsize=16)
 
     fig.subplots_adjust(wspace=.4,top=.85)
-    plt.savefig(create_size_folderdata(size)+"/"+"plot_configs_size=%d_center=(%d_%d)_std=%dt_end=%d.png"%(size,center[0],center[1],std,t_end))
+    plt.savefig(create_size_folderdata(size)+"/"+"plot_configs_size=%d_center=(%d_%d)_std=%d_dt=%.5f_t_end=%d.png"%(size,center[0],center[1],std,dt,t_end))
     plt.show()
     
     
     
-    return result_path_name+"/"+name_data(t_end), saved_counter
+    return result_path_name+"/"+name_data(t_end,dt), saved_counter
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
@@ -358,19 +388,20 @@ def time_ev_mat(grid,t_end=500,dt=.1):
 
 #shift order to simulate test or paper configuration
 
+
+size = 30
+std = size/5
+center = [size/8,size/3]
+
 size = 300
 std = 266/4
 center = [133,133]
 
 
-size = 100
-std = size/5
-center = [size/8,size/3]
 
 
-
-dt = .1
-t_end = 10000
+dt = .005
+t_end = 400000
 
 grid = convert_to_df(makeGaussian(size=size,center=center, fwhm = std))
 plt.matshow(grid)
@@ -379,15 +410,16 @@ plt.show()
 output_path = time_ev_mat(grid,t_end=t_end,dt=dt) #execute and returns path of data output 
 
 
-# In[65]:
+# In[ ]:
 
 
 def display_sequence_contourf_out(output_path,t_end):
     """Slider implementation of visualization over time"""
-    data = get_data(output_path[0],t_end)
     def _show(frame=(0, t_end-1)):
+        data = get_data(output_path[0],t_end)
+        plt.figure(figsize=[7,7])
         plt.style.use('seaborn-white')
-        plt.title("%d step"%(frame*100*dt),y=1.1)#n = save every
+        plt.title("%d frame (t = %.3f)"%(int(frame*(t_end/(dt*100))*dt),frame*1000*dt),y=1.1)#n = save every
         plt.contourf(get_configuration_out(data,frame),10,cmap='Spectral') # n equally spaced lines
         plt.colorbar()
         #plt.clim(0,1)
@@ -399,7 +431,7 @@ def get_configuration_out(data,t):
     return convert_to_df(np.asarray(data.iloc[t]).reshape(size,size))
 
 
-# In[66]:
+# In[ ]:
 
 
 #data = get_data(output_path[0],t_end)
@@ -413,19 +445,19 @@ display_sequence_contourf_out(output_path,output_path[1])
 
 
 
-# In[52]:
+# In[ ]:
 
 
 
 
 
-# In[53]:
+# In[ ]:
 
 
 
 
 
-# In[73]:
+# In[ ]:
 
 
 # data = get_data(output_path[0],output_path[1])
@@ -489,20 +521,22 @@ display_sequence_contourf_out(output_path,output_path[1])
 # plt.show()
 
 
-# In[ ]:
+# In[82]:
 
 
 
 
 
-# In[ ]:
+# In[117]:
 
 
+x = np.full(shape=[3,3],fill_value=1.,dtype="float32")
+y = np.full(shape=[3,3],fill_value=-3.3,dtype="float32")
 
 
-
-# In[ ]:
-
+# In[111]:
 
 
-
+from scipy.linalg import get_blas_funcs
+gemm = get_blas_funcs("gemm", [x, y])
+gemm(3, y, x) 
