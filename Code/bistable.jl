8b5f2d75118c7741c69c832ed14afe83e71b9280
Sergio <sergiopicella@gmail.com>
2020-03-29 00:24:30 +0100

    Update bistable.jl

--- Code/bistable.jl
+++ Code/bistable.jl
@@ -30,14 +30,12 @@ end
 c1,c2,c3,c4,c5,c6,β,γ=3e3,1.1e4,1e-3,3e3,1.1e4,1e-3,2,2 #from paper
 function weight_matrices(N)
       react1 = function (y) return c1/(c2+(y^β)) end
-      react2 = function (x) return c3*x end
       react3 = function (x) return c4/(c5+(x^γ)) end
-      react4 = function (y) return c6*y end
 
       TWx1 = zeros(Float32,N^2,N^2)
       TWox1 = zeros(Float32,N^2,N^2)
       TWx2 = zeros(Float32,N^2,N^2)
-      TWxo2 = zeros(Float32,N^2,N^2)
+      TWox2 = zeros(Float32,N^2,N^2)
 
       for i in 1:N
             for j in 1:N
@@ -54,15 +52,13 @@ function weight_matrices(N)
       return TWx1, TWox1, TWx2, TWox2
 end
 
-N = 300
-
+N = 200
 W_mats = weight_matrices(N)
 TWx1, TWox1, TWx2, TWox2 = W_mats[1], W_mats[2], W_mats[3], W_mats[4]
 W_mats = nothing
-#check uselss are empty
-TWx2
-W_mats
-Plots.heatmap(TWx2)
+
+#Plots.heatmap(TWx2)
+
 ##
 E_1 = zeros(Float32,N,N)
 Em1 = zeros(Float32,N,N)
@@ -89,22 +85,35 @@ Em1=nothing
 E_1=nothing
 
 J1 = T10 * TWox2
-J2 = c3 *  Tm10 * TWx1
-
-J3 = c3 * TWx1
-J4 = T01 * TWox1
-J5 = c6 *  T0m1 * TWx2
-J6 = c6 * TWx2
-
-TWx1=nothing
-TWx2=nothing
 T10=nothing
-T01=nothing
-T0m1=nothing
+
+J2 = Tm10 * TWx1
+J2 = c3 .*J2  
 Tm10=nothing
+
+J3 = c3 .* TWx1
+TWx1=nothing
+
+J4 = T01 * TWox1
+T01=nothing
+
+J5 = T0m1 * TWx2
+J5 = c6 .* J5  
+T0m1=nothing
+
+J6 = c6 .* TWx2
+TWx2=nothing
 
 J = J1.+J2.-TWox2.-J4.+J4.+J5-TWox1.-J6
 
+J1=nothing
+J2=nothing
+J3=nothing
+J4=nothing
+J5=nothing
+J6=nothing
+TWox1=nothing
+TWox2=nothing
 #J =   T10 * TWox2  .+ c3 *  Tm10 * TWx1 .- TWox2 .- c3 * TWx1.+
 #      .+   T01 * TWox1  .+ c6 *  T0m1 * TWx2 .- TWox1 .- c6 * TWx2
 
@@ -112,7 +121,7 @@ Plots.heatmap(J)
 
 #MatLab: ode113 => Julia: Vern7()
 grid0 = config0(300,[133,133],[133,133])[2]
-#grid0 = config0(N,[N/3,N/6],[N,N/2])[2]
+grid0 = config0(N,[N/3,N/6],[N,N/2])[2]
 Plots.heatmap(grid0)
 grid_diag = diag(grid0)
 
@@ -120,15 +129,16 @@ grid_1darray = reshape(grid0,N^2,1)
 J*grid_1darray
 
 u0 = grid_1darray
-tspan = (0.0,1e5)
+tspan = (0.0,1e2)
 f(u,p,t) = J*u
 prob = ODEProblem(f,u0,tspan)
 #sol = solve(prob,Vern7(),abstol=1e-100,reltol=3e-14)
-sol = solve(prob,Vern7(),abstol=1e-8,reltol=3e-6)
-#sol = solve(prob,Vern7())
+#sol = solve(prob,Vern7(),abstol=1e-8,reltol=3e-6)
+sol = solve(prob,Vern7())
 
 
 length(sol)
 
 grid_out = reshape(sol[length(sol)],N,N)
 Plots.heatmap(grid_out)
+
